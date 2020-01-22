3bcaef26a1cb600e9ba7cdc7aeaf2bf466167e77
sanwailwin <sanwailwin21@gmail.com>
2020-01-22 14:57:16 +0630

    remove no need and fix error

--- src/components/InstructorApp.jsx
+++ src/components/InstructorApp.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import ListCoursesComponent from '../component/ListCoursesComponent';
-import CourseComponent from '../component/CourseComponent';
-import ListTutorialsComponent from '../component/tutorial/ListTutorialsComponent';
-import TutorialComponent from '../component/tutorial/TutorialComponent';
+import ListTutorialsComponent from '../components/tutorial/ListTutorialsComponent';
+import TutorialComponent from '../components/tutorial/TutorialComponent';
 
 class InstructorApp extends React.Component {
     render() {
@@ -13,9 +11,7 @@ class InstructorApp extends React.Component {
                 <>
                     <h1>Instructor Application</h1>
                     <Switch>
-                        <Route path="/" exact component={ListCoursesComponent} />
-                        <Route path="/courses" exact component={ListCoursesComponent} />
-                        <Route path="/courses/:id" component={CourseComponent} />
+                        <Route path="/" exact component={ListTutorialsComponent} />
                         <Route path="/tutorials" exact component={ListTutorialsComponent} />
                         <Route path="/tutorials/:id" component={TutorialComponent} />
                     </Switch>
